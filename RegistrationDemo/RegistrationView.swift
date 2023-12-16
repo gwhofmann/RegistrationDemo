38be150437906e78b4c50ac568fef8debd1ec3bb
gregor <gregor.hofmann@gmail.com>
2023-12-16 15:48:48 +0100

    Implement a first version of the registration input view, including text fields, a date picker, and a registration button.

--- RegistrationDemo/RegistrationView.swift
+++ RegistrationDemo/RegistrationView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @ObservedObject var registrationHandler: RegistrationHandler
+    
+    @State private var nameEntry = ""
+    @State private var emailEntry = ""
+    @State private var dobEntry = Date.now
+    
+    func commitInput() {
+        guard registrationHandler.isValidName(name: nameEntry) &&
+                registrationHandler.isValidEmail(email: emailEntry) &&
+                registrationHandler.isValidDateOfBirth(dateOfBirth: dobEntry) else {
+            return
+        }
+        registrationHandler.registrationData = RegistrationData(name: nameEntry, email: emailEntry, dateOfBirth: dobEntry)
+        registrationHandler.persistRegistrationData()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Registrierung").font(.title)
+            Spacer()
+            Text("Um dich zu registrieren, gib' bitte deinen Namen, deine Email-Adresse und dein Geburtsdatum ein.").padding()
+            TextField("Name", text: $nameEntry)
+                .textFieldStyle(.roundedBorder)
+                .textContentType(.name)
+                .keyboardType(.default)
+                .autocorrectionDisabled()
+                .padding(.horizontal).padding()
+            TextField("Email-Adresse", text: $emailEntry)
+                .textFieldStyle(.roundedBorder)
+                .textContentType(.emailAddress)
+                .keyboardType(.emailAddress)
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+                .padding(.horizontal)
+                .padding()
+            DatePicker("Geburtsdatum:", selection: $dobEntry, displayedComponents: .date)
+                .padding(.horizontal)
+                .padding()
+            Spacer()
+            Button(action: commitInput){
+                Text("Registrieren").font(.title2)
+            }.padding()
+            
+        }
     }
 }
 
 struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
-        RegistrationView()
+        RegistrationView(registrationHandler: RegistrationHandler())
     }
 }
