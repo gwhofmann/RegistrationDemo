69133cebe2b9c7c65cbaebd299f91b7733ea4fac
gregor <gregor.hofmann@gmail.com>
2023-12-16 13:45:20 +0100

    Initial Commit

---
+++ RegistrationDemo/RegistrationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationDemoApp.swift
+//  RegistrationDemo
+//
+//  Created by Gregor Hofmann on 16.12.23.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
