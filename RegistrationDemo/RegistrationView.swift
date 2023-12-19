2a265e0caa5a7fccad1c014a36f83b80c2b8c1f2
gregor <gregor.hofmann@gmail.com>
2023-12-19 13:50:25 +0100

    Improvements to registration screen UI: text input fields now have a clearer border that changes colour to red on bad input. The date prompt now also changes colour to red when an invalid date is submitted for registration. Placeholder text in the input fields is more legible, particularly in dark mode. Added slight animations for the warning text and colour change when data is submitted to the name and email input fields. Removed onSubmit trigger for the DatePicker, as it wasn't working as intended.

--- RegistrationDemo/RegistrationView.swift
+++ RegistrationDemo/RegistrationView.swift
@@ -50,29 +50,39 @@ struct RegistrationView: View {
                 Text("Registrierung").font(.title)
                 Text("Um dich zu registrieren, gib' bitte deinen Namen, deine Email-Adresse und dein Geburtsdatum ein.").padding(.vertical)
                 VStack{
-                    TextField("Name", text: $nameEntry)
+                    TextField("", text: $nameEntry, prompt: Text("Name").foregroundColor(Color("InputPlaceholderColour")))
                         .onSubmit {
-                            nameEntryAttempted = true
+                            withAnimation{
+                                nameEntryAttempted = true
+                            }
                         }
-                        .textFieldStyle(.roundedBorder)
+                        .textFieldStyle(.plain)
                         .textContentType(.name)
                         .keyboardType(.default)
                         .autocorrectionDisabled()
+                        .padding().background(RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1.5)
+                            .foregroundColor(nameEntryAttempted && !nameEntryIsValid ? .red : .primary))
                         .padding(.horizontal)
                     if nameEntryAttempted && !nameEntryIsValid {
                         Text("Bitte gib' einen Namen ein.").foregroundColor(.red)
                     }
                 }.padding()
                 VStack{
-                    TextField("Email-Adresse", text: $emailEntry)
+                    TextField("", text: $emailEntry, prompt: Text("Email-Adresse").foregroundColor(Color("InputPlaceholderColour")))
                         .onSubmit {
-                            emailEntryAttempted = true
+                            withAnimation{
+                                emailEntryAttempted = true
+                            }
                         }
-                        .textFieldStyle(.roundedBorder)
+                        .textFieldStyle(.plain)
                         .textContentType(.emailAddress)
                         .keyboardType(.emailAddress)
                         .autocorrectionDisabled()
                         .textInputAutocapitalization(.never)
+                        .padding().background(RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1.5)
+                            .foregroundColor(emailEntryAttempted && !emailEntryIsValid ? .red : .primary))
                         .padding(.horizontal)
 
                     if emailEntryAttempted && !emailEntryIsValid {
@@ -80,11 +90,10 @@ struct RegistrationView: View {
                     }
                 }.padding()
                 VStack{
-                    DatePicker("Geburtsdatum:", selection: $dobEntry, displayedComponents: .date)
-                        .onSubmit {
-                            dobEntryAttempted = true
-                        }
-                        .padding(.horizontal)
+                    DatePicker(selection: $dobEntry, displayedComponents: .date){
+                        Text("Geburtsdatum:")                            .foregroundColor(dobEntryAttempted && !dobEntryIsValid ? .red : .primary)
+                    }
+                    .padding(.horizontal)
                     
                     if dobEntryAttempted && !dobEntryIsValid {
                         Text("Bitte setze ein gültiges Geburtsdatum zwischen dem 1.1.1900 und dem 31.12.2022.").foregroundColor(.red)
